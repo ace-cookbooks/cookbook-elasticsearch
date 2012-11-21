679bbe6c141a806571f6001c78d5370455972051
Ryan Schlesinger <ryan@instanceinc.com>
2012-11-21 10:40:26 -0800

    Being abundantly clear about this hash

--- recipes/monit.rb
+++ recipes/monit.rb
@@ -3,7 +3,7 @@
 begin
   monitrc "elasticsearch" do
     template_cookbook "elasticsearch"
-    variables(:pidfile => "#{node.elasticsearch[:pid_path]}/#{node.elasticsearch[:node_name].to_s.gsub(/\W/, '_')}.pid")
+    variables({:pidfile => "#{node.elasticsearch[:pid_path]}/#{node.elasticsearch[:node_name].to_s.gsub(/\W/, '_')}.pid"})
   end
 rescue Exception => e
   Chef::Log.error "The 'monit' recipe is not included in the node run_list or the 'monitrc' resource is not defined"
